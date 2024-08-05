e57bdf763c3c51cce915b11a7a76cc11e616527d
unre <61712742+unreelcy@users.noreply.github.com>
2024-08-05 18:14:01 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = '7457979572:AAH89rfdFWj2iWDK6OhaLSueEPW6a5TRI_E'
-file_directory_download = 'Downloaded_files/'
-file_directory_convert = 'Converted_files/'
-file_directory_marks = 'Marked_pdfs/'
