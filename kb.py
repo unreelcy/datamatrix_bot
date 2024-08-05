f73c079fdc050f34a7d4cc7b0524528b470f700e
unre <61712742+unreelcy@users.noreply.github.com>
2024-08-05 18:14:16 +0300

    Delete kb.py

--- kb.py
+++
@@ -1,8 +0,0 @@
-# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-# menu = [[
-#         InlineKeyboardButton(text="Отправить pdf файлом", callback_data="to_pdf"),
-#         InlineKeyboardButton(text="Отправить изображением", callback_data="to_png")]]
-#
-# menu = InlineKeyboardMarkup(inline_keyboard=menu)
-# exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
-# iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
